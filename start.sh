d5f44f65d5355ae3e38026479db2620768daecd1
Yasser Nabi <yassersaleemi@gmail.com>
2014-10-23 01:13:31 +0100

    Generate new SSH host keys
    Big oversight :( Host keys are identical in images. This commit forces
    the regeneration of host keys when docker images are run.

--- start.sh
+++ start.sh
@@ -2,6 +2,18 @@
 
 env | egrep "^JENKINS|^TZ" > /docker.env
 
+# Generate SSH host keys
+rm -f "/etc/ssh/ssh_host_key"
+rm -f "/etc/ssh/ssh_host_rsa_key"
+rm -f "/etc/ssh/ssh_host_dsa_key"
+rm -f "/etc/ssh/ssh_host_ecdsa_key"
+rm -f "/etc/ssh/ssh_host_ed25519_key"
+ssh-keygen -N '' -q -f "/etc/ssh/ssh_host_key" -t rsa1
+ssh-keygen -N '' -q -f "/etc/ssh/ssh_host_rsa_key" -t rsa
+ssh-keygen -N '' -q -f "/etc/ssh/ssh_host_dsa_key" -t dsa
+ssh-keygen -N '' -q -f "/etc/ssh/ssh_host_ecdsa_key" -t ecdsa
+ssh-keygen -N '' -q -f "/etc/ssh/ssh_host_ed25519_key" -t ed25519
+
 MONIT_PASSWD=$(openssl rand -base64 6)
 echo MONIT_PASSWORD=${MONIT_PASSWD}
 
